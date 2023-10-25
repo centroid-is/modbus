bc4c34d2e68533eed950281d609eee8a91a19106
Ómar Högni Guðmarsson <kind@rolla.me>
2023-10-25 11:09:24 +0000

    Clang format

--- src/error.cpp
+++ src/error.cpp
@@ -28,36 +28,52 @@
 namespace modbus {
 
 namespace {
-    /// Error category for modbus errors.
-    class modbus_category_t : public std::error_category {
-        /// Get the name of the error category
-        [[nodiscard]] auto name() const noexcept -> char const * override { return "modbus"; }
+/// Error category for modbus errors.
+class modbus_category_t : public std::error_category {
+  /// Get the name of the error category
+  [[nodiscard]] auto name() const noexcept -> char const* override { return "modbus"; }
 
-        /// Get a descriptive error message for an error code.
-        [[nodiscard]] auto message(int error) const noexcept -> std::string override {
-            switch (static_cast<errc::errc_t>(error)) {
-                case errc::no_error:                                 return "error 00: No error - internal";
-                case errc::illegal_function:                         return "error 01: Illegal function";
-                case errc::illegal_data_address:                     return "error 02: Illegal data address";
-                case errc::illegal_data_value:                       return "error 03: Illegal data value";
-                case errc::server_device_failure:                    return "error 04: Server device failure";
-                case errc::acknowledge:                              return "error 05: Acknowledge";
-                case errc::server_device_busy:                       return "error 06: Server device busy";
-                case errc::memory_parity_error:                      return "error 08: Memory parity error";
-                case errc::gateway_path_unavailable:                 return "error 10: Gateway path unavailable";
-                case errc::gateway_target_device_failed_to_respond:  return "error 11: Gateway target device failed to respond";
-                case errc::message_size_mismatch:                    return "peer error: message size mismatch";
-                case errc::message_too_large:                        return "peer error: message size limit exceeded";
-                case errc::unexpected_function_code:                 return "peer error: unexpected function code";
-                case errc::invalid_value:                            return "peer error: invalid value received";
-            }
+  /// Get a descriptive error message for an error code.
+  [[nodiscard]] auto message(int error) const noexcept -> std::string override {
+    switch (static_cast<errc::errc_t>(error)) {
+      case errc::no_error:
+        return "error 00: No error - internal";
+      case errc::illegal_function:
+        return "error 01: Illegal function";
+      case errc::illegal_data_address:
+        return "error 02: Illegal data address";
+      case errc::illegal_data_value:
+        return "error 03: Illegal data value";
+      case errc::server_device_failure:
+        return "error 04: Server device failure";
+      case errc::acknowledge:
+        return "error 05: Acknowledge";
+      case errc::server_device_busy:
+        return "error 06: Server device busy";
+      case errc::memory_parity_error:
+        return "error 08: Memory parity error";
+      case errc::gateway_path_unavailable:
+        return "error 10: Gateway path unavailable";
+      case errc::gateway_target_device_failed_to_respond:
+        return "error 11: Gateway target device failed to respond";
+      case errc::message_size_mismatch:
+        return "peer error: message size mismatch";
+      case errc::message_too_large:
+        return "peer error: message size limit exceeded";
+      case errc::unexpected_function_code:
+        return "peer error: unexpected function code";
+      case errc::invalid_value:
+        return "peer error: invalid value received";
+    }
 
-            return "unknown error: " + std::to_string(error);
-        }
-    } modbus_category_instance;
-} // namespace
+    return "unknown error: " + std::to_string(error);
+  }
+} modbus_category_instance;
+}  // namespace
 
 /// The error category for modbus errors.
-auto modbus_category() -> std::error_category const & { return modbus_category_instance; }
+auto modbus_category() -> std::error_category const& {
+  return modbus_category_instance;
+}
 
-} // namespace modbus
+}  // namespace modbus
